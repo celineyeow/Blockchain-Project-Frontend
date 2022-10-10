a85ec049edacb6f6e7d44b2aed2fcc2c5d686868
panda3141592 <39628281+panda3141592@users.noreply.github.com>
2022-10-10 22:10:55 +0800

    Interacting with Solidity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { render } from "react-dom";
+// React
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Paths
 import Home from './pages/Home.js'
 import About from "./pages/About.js";
 import NotFound from "./pages/NotFound.js";
 
+// Web3
 import Web3 from 'web3';
+import { CONTRACT_NAME_ADDRESS, CONTRACT_NAME_ABI } from "./contracts/config";
 
 function App() {
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+  const contract = new web3.eth.Contract(CONTRACT_NAME_ABI, CONTRACT_NAME_ADDRESS);
+
   return(
     <div className="App">
         <BrowserRouter>
---
+++ src/contracts/config.js
@@ -0,0 +1,53 @@
+export const CONTRACT_NAME_ADDRESS = "0x469d00881151499890A09DD073d3d613bF21e591"
+export const CONTRACT_NAME_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_n",
+				"type": "uint256"
+			}
+		],
+		"name": "setNP",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_n",
+				"type": "uint256"
+			}
+		],
+		"name": "setP",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_p",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
